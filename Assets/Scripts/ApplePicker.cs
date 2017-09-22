4883714597552acda9ca4138a5cae9d4c01a63ab
CzipoM <Mikecz613@aol.com>
2017-09-22 00:20:46 -0400

    Baskets will now be deleted when apples are missed

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -8,14 +8,18 @@ public class ApplePicker : MonoBehaviour {
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+
 
 	void Start () {
+        basketList = new List<GameObject>();
 		for(int i=0; i <numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY + i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
 	}
 	
@@ -26,6 +30,11 @@ public class ApplePicker : MonoBehaviour {
         {
             Destroy(tGO);
         }
+
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 	
 }
