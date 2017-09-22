82dc3f23ff2de6e75ab5742848237625085c6c9b
CzipoM <Mikecz613@aol.com>
2017-09-21 23:12:17 -0400

    Apple Tree will randomly move left and right at the top of the screen

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -6,11 +6,11 @@ public class AppleTree : MonoBehaviour {
 
     public GameObject applePrefab;
 
-    public float speed = 1f;
+    public float speed = 10f;
 
     public float leftAndRightEdge = 20f;
 
-    public float chanceToChangeDirections = 0.1f;
+    public float chanceToChangeDirections = 0.02f;
 
     public float secondsBetweenAppleDrops = 1f;
 
@@ -19,6 +19,22 @@ public class AppleTree : MonoBehaviour {
     }
 
     void Update(){
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }else if(pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+    void FixedUpdate()
+    {
+        if(Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
